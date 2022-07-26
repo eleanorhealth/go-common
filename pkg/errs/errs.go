5db9e6be2675cb60cf08a8a795292225cacb0639
Will Edgington <will.edgington@gmail.com>
2022-07-25 20:06:00 -0400

    Return nil if error is nil in errs.Wrap() and errs.Wrapf()

--- pkg/errs/errs.go
+++ pkg/errs/errs.go
@@ -15,9 +15,17 @@ func Cause(err error) error {
 }
 
 func Wrap(err error, msg string) error {
+	if err == nil {
+		return nil
+	}
+
 	return fmt.Errorf("%s: %w", msg, err)
 }
 
 func Wrapf(err error, format string, args ...any) error {
+	if err == nil {
+		return nil
+	}
+
 	return Wrap(err, fmt.Sprintf(format, args...))
 }
