dbc7f5b6eca7451a65d9ee36691192a26b99e2ff
dillonstreator <dillonstreator@gmail.com>
2023-06-02 09:30:20 -0500

    enable custom span configuration per traced transport

--- pkg/infra/http.go
+++ pkg/infra/http.go
@@ -8,7 +8,24 @@ import (
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/ext"
 )
 
-func HTTPTracedTransport(rt http.RoundTripper, serviceName string) http.RoundTripper {
+type beforeFn func(req *http.Request, span ddtrace.Span)
+type config struct {
+	before beforeFn
+}
+type OptionFn func(c *config)
+
+func WithBefore(before beforeFn) OptionFn {
+	return func(c *config) {
+		c.before = before
+	}
+}
+
+func HTTPTracedTransport(rt http.RoundTripper, serviceName string, optionFns ...OptionFn) http.RoundTripper {
+	cfg := &config{}
+	for _, optionFn := range optionFns {
+		optionFn(cfg)
+	}
+
 	return httptrace.WrapRoundTripper(rt, []httptrace.RoundTripperOption{
 		httptrace.WithBefore(func(req *http.Request, span ddtrace.Span) {
 			span.SetTag(ext.ServiceName, serviceName)
@@ -17,6 +34,9 @@ func HTTPTracedTransport(rt http.RoundTripper, serviceName string) http.RoundTri
 			span.SetTag(ext.HTTPURL, req.URL.Path)
 			span.SetTag(ext.TargetHost, req.URL.Hostname())
 			span.SetTag(ext.HTTPUserAgent, req.UserAgent())
+			if cfg.before != nil {
+				cfg.before(req, span)
+			}
 		}),
 	}...)
 }
