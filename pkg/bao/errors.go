bfee98b61940f752c0a0a074544b74ae08627f50
Thom Ribeiro <thomazella9@gmail.com>
2024-04-28 21:08:21 -0300

    validateID, cleanup internal relatedOp type

--- pkg/bao/errors.go
+++ pkg/bao/errors.go
@@ -5,3 +5,5 @@ import "errors"
 var ErrModelNotStructOrSlice = errors.New("model must be a struct or a slice")
 var ErrModelNotStruct = errors.New("model must be a pointer to a struct")
 var ErrOnePrimaryKey = errors.New("table must have exactly one primary key")
+var ErrUpdateNotExists = errors.New("model to be updated does not exist")
+var ErrIDNotUUID = errors.New("id must be a valid UUID")
