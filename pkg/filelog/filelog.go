4f16cc610eecc6fa5245b2a6f032cded7e8d346e
Thomazella Ribeiro <thomazella9@gmail.com>
2024-05-24 10:34:46 -0400

    Add filelog package (#40)

---
+++ pkg/filelog/filelog.go
@@ -0,0 +1,42 @@
+package filelog
+
+import (
+	"io"
+	"os"
+	"strings"
+
+	"github.com/eleanorhealth/go-common/pkg/errs"
+)
+
+type Logger interface {
+	Log(msg string) error
+}
+
+type Nop struct{}
+
+var _ Logger = Nop{}
+
+func (Nop) Log(msg string) error {
+	return nil
+}
+
+type FileLogger struct {
+	Path string
+}
+
+var _ Logger = FileLogger{}
+
+func (f FileLogger) Log(msg string) error {
+	file, err := os.OpenFile(f.Path, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	if err != nil {
+		return errs.Wrap(err, "unable to open file")
+	}
+
+	defer file.Close()
+	_, err = io.Copy(file, strings.NewReader(msg))
+	if err != nil {
+		return errs.Wrap(err, "unable to write to file")
+	}
+
+	return nil
+}
