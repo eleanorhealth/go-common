21b380ecab8000b4fdac69b5177f9c1062eb21ac
Raphael "Thom" Thomazella <thomazella9@gmail.com>
2026-03-11 01:36:23 -0300

    misc: fix tests, fix vulnerabilities

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM golang:1.26-alpine
 
-RUN apk --no-cache add libc-dev gcc git
+RUN apk upgrade --no-cache && \
+    apk --no-cache add libc-dev gcc bash git
 
 RUN apk update && \
-    apk upgrade libssl3 libcrypto3
+    apk upgrade libssl3 libcrypto3 zlib
 
 ENV CGO_ENABLED 1
 
--- pkg/infra/db_test.go
+++ pkg/infra/db_test.go
@@ -13,9 +13,9 @@ import (
 func testDB(t *testing.T) *sql.DB {
 	assert := assert.New(t)
 
-	dsn := env.Get("POSTGRES_DSN", "")
+	dsn := env.Get("POSTGRES_TEST_DSN", "")
 	if len(dsn) == 0 {
-		assert.FailNow("POSTGRES_DSN is empty")
+		assert.FailNow("POSTGRES_TEST_DSN is empty")
 	}
 
 	db, err := DB(context.Background(), dsn, "")
